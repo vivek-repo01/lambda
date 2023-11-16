6b0691a6cb107bd93e5ec31dc722f839eaae600d
vivek-repo01 <147979512+vivek-repo01@users.noreply.github.com>
2023-11-16 19:54:56 +0530

    Add files via upload

---
+++ sort a list of dictionaries using Lambda.py
@@ -0,0 +1,13 @@
+#Write a Python program to sort a list of dictionaries using Lambda.
+#Original list of dictionaries :
+#[{'make': 'Nokia', 'model': 216, 'color': 'Black'}, {'make': 'Mi Max', 'model': '2', 'color': 'Gold'}, {'make': 'Samsung', 'model': 7, 'color': 'Blue'}]
+#Sorting the List of dictionaries :
+#[{'make': 'Nokia', 'model': 216, 'color': 'Black'}, {'make': 'Samsung', 'model': 7, 'color': 'Blue'}, {'make': 'Mi Max', 'model': '2', 'color': 'Gold'}]
+
+models = [{'make':'Nokia', 'model':216, 'color':'Black'}, {'make':'Mi Max', 'model':'2', 'color':'Gold'}, {'make':'Samsung', 'model': 7, 'color':'Blue'}]
+print("Original list of dictionaries :")
+print(models)
+sorted_models = sorted(models, key = lambda x: x['color'])
+print("\nSorting the List of dictionaries :")
+print(sorted_models)
+
